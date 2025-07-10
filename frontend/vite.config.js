7443872a06e2d8deb0ddc363b11a5b003e69f227
Arielgsp <campa990@gmail.com>
2025-07-10 01:45:11 -0300

    Configuración para despliegue

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,29 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
-import json from "@rollup/plugin-json";
 
 export default defineConfig({
-  plugins: [react(), json()],
+  plugins: [react()],
   resolve: {
     alias: {
-      "@": resolve(__dirname, "./src"),
+      "@": resolve(__dirname, "src"),
+      "/Frontend/src": resolve(__dirname, "src"),
+    },
+  },
+  root: ".",
+  base: "./",
+  build: {
+    outDir: "../dist",
+    emptyOutDir: true,
+    rollupOptions: {
+      input: resolve(__dirname, "index.html"),
+    },
+  },
+  server: {
+    port: 5173,
+    strictPort: true,
+    fs: {
+      strict: false,
     },
   },
 });
